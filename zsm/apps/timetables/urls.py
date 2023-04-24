06d05c44a32f778e7567daff5e3b089388740383
zotkinsb <zotkinsb@icloud.com>
2023-04-24 06:56:08 +0300

    Feedback block modded

--- zsm/apps/timetables/urls.py
+++ zsm/apps/timetables/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<slug:tt_slug>/', views.detail, name='detail'),
-    path('search/', views.Search.as_view(), name='search ')
+    path('search/', views.Search.as_view(), name='search')
 ]
