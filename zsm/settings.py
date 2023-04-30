3c6bf140a947048b32d98620398a9065b6468ca6
zotkinsb <zotkinsb@icloud.com>
2023-04-30 17:32:07 +0300

    postgresql setup done may be

--- zsm/settings.py
+++ zsm/settings.py
@@ -81,10 +81,21 @@ WSGI_APPLICATION = 'zsm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'zotkinsb',
+        'USER': 'zotkinsb',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
+
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
